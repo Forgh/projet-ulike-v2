ebb1664fa665e5e9041a4c7a4677a267bf62fc5b
mcflying.marty@gmail.com <mcflying.marty@gmail.com@a6f2d0cb-316a-46ee-b7cf-88ad128f2803>
2013-12-01 14:59:49 +0000

    correction syntaxe dans erreur_login.php

--- erreur_login.php
+++ erreur_login.php
@@ -11,17 +11,17 @@
 	
 	<div id="bodycentered" >
 		<?php
-			if(isset($_POST['login_err'])){
+			if(isset($_SESSION['login_err'])){
 		?>
-			<h2><?php echo $_POST['login_err']; ?></h2>
-			<p><?php echo $_POST['login_err_msg']; ?></p>
+			<h2><?php echo $_SESSION['login_err']; ?></h2>
+			<p><?php echo $_SESSION['login_err_msg']; ?></p>
 		<?php
 			}else{
 		?>
 			<h2>Aucune erreur</h2>
 			<p>Aucune erreur détecté.</p>
 		<?php } 
-			$_POST['login_err'] = null;
+			$_SESSION['login_err'] = null;
 		?>
 	</div>
 	
