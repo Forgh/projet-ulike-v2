5a2ce3a14d5417a365c993a075396591b197f294
mcflying.marty@gmail.com <mcflying.marty@gmail.com@a6f2d0cb-316a-46ee-b7cf-88ad128f2803>
2013-12-01 15:37:21 +0000

    version de test sur erreur_login.php, loguer_entreprise.php, loguer_membre.php

--- erreur_login.php
+++ erreur_login.php
@@ -4,14 +4,14 @@
 
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="refresh" content="5;url=http://projets-lightdark.fr/login.php" >
+	<meta http-equiv="refresh" content="5;url=http://projets-lightdark.fr/ulike/login.php" >
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
 	
 	<div id="bodycentered" >
 		<?php
-			if(!empty($_SESSION['login_err'])){
+			/*if(!empty($_SESSION['login_err'])){
 		?>
 			<h2><?php echo $_SESSION['login_err']; ?></h2>
 			<p><?php echo $_SESSION['login_err_msg']; ?></p>
@@ -21,8 +21,10 @@
 			<h2>Aucune erreur</h2>
 			<p>Aucune erreur détecté.</p>
 		<?php } 
-			$_SESSION['login_err'] = null;
+			$_SESSION['login_err'] = null;*/
 		?>
+		<h2>ARGGGGG</h2>
+			<p>Erreur</p>
 	</div>
 	
 </body>
