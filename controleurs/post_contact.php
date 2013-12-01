1558512a59cd62540003acf85f154b99115dfb21
mcflying.marty@gmail.com <mcflying.marty@gmail.com@a6f2d0cb-316a-46ee-b7cf-88ad128f2803>
2013-12-01 01:36:38 +0000

    correction dans post_contact.php, mail.php

--- controleurs/post_contact.php
+++ controleurs/post_contact.php
@@ -1,10 +1,8 @@
 <?php
 include('../modeles/mail.php');
-$to=htmlspecialchars('mcflying.marty@gmail.com');
+$to=htmlspecialchars('bob@yopmail.com');
 $sujet='Contact ULike : M/Mme '.$_POST['sujet'];
 $corps='Une nouvelle question vous a été posé : '.$_POST['texte'];
-$headers = 'From: ' . $_POST['mail']. '\r\n' .
-'Reply-To: prenom.nom@univ-montp2.fr' . '\r\n' .
-'X-Mailer: PHP/' . phpversion();
-mail($to,$sujet,$corps,$headers);
+
+envoyer_mail($to,$_POST['mail'],$sujet,$corps);
 ?>
