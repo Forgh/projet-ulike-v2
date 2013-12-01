d450cec3f8b8acca87abfe56f01e508f88bb8a30
mcflying.marty@gmail.com <mcflying.marty@gmail.com@a6f2d0cb-316a-46ee-b7cf-88ad128f2803>
2013-12-01 01:26:05 +0000

    modif de mail.php, post_contact.php

--- trunk/controleurs/post_contact.php
+++ trunk/controleurs/post_contact.php
@@ -1,6 +1,6 @@
 <?php
 include('../modeles/mail.php');
-$to=htmlspecialchars('bob@yopmail.com');
+$to=htmlspecialchars('mcflying.marty@gmail.com');
 $sujet='Contact ULike : M/Mme '.$_POST['nom'];
 $corps='Une nouvelle question vous a été posé : '.$_POST['texte'];
 $headers = 'From: ' . $_POST['mail']. '\r\n' .
