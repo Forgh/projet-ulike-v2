02950f707793a70f8a2d62a1367b0727a4ffc5a4
mcflying.marty@gmail.com <mcflying.marty@gmail.com@a6f2d0cb-316a-46ee-b7cf-88ad128f2803>
2013-12-01 01:30:01 +0000

    correction potentiel du bug du mail dans post_contact.php

--- controleurs/post_contact.php
+++ controleurs/post_contact.php
@@ -1,7 +1,7 @@
 <?php
 include('../modeles/mail.php');
 $to=htmlspecialchars('mcflying.marty@gmail.com');
-$sujet='Contact ULike : M/Mme '.$_POST['nom'];
+$sujet='Contact ULike : M/Mme '.$_POST['sujet'];
 $corps='Une nouvelle question vous a été posé : '.$_POST['texte'];
 $headers = 'From: ' . $_POST['mail']. '\r\n' .
 'Reply-To: prenom.nom@univ-montp2.fr' . '\r\n' .
